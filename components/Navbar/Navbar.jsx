717752c18888eafcd362127a359b7baafeef6e5d
Christian <c.espinoza.code@gmail.com>
2023-07-15 16:32:39 -0500

    initial html structure created for navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+        </li>
+        <li>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export { Navbar }
