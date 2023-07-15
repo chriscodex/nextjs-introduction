a89b232b3895f4d85a807679f79b5101f8ef95d0
Christian <c.espinoza.code@gmail.com>
2023-07-15 16:33:47 -0500

    dimamic page created

---
+++ pages/product/[id].jsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+function productId() {
+  const router = useRouter()
+
+  return (
+    <div>
+      Pagina dinamica: 
+    </div>
+  )
+}
+
+export default productId
