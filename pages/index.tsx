db568570304e9d12c66eef28d8c39de41d790b46
Christian <c.espinoza.code@gmail.com>
2023-07-17 04:41:48 -0500

    Server side rendering added to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,18 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch(`${process.env.API_HOST}/api/avo`)
-//   const { data: productList }: TAPIAvoResponse = await response.json()
+export const getServerSideProps = async () => {
+  const response = await fetch('https://nextjs-introduction.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-//   return {
-//     props: {
-//       productList,
-//     },
-//   }
-// }
-
-
-
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([]);
-
-  const apiPetition = async () => {
-    const response = await fetch('/api/avo')
-    const {data, length} = await response.json()
-    console.log(data);
-    setProductList(data)
+  return {
+    props: {
+      productList,
+    },
   }
-  
-  useEffect(() => {
-    apiPetition()
-  },[])
+}
 
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
